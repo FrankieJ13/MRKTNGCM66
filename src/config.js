1e224bf0bc9739f5c624079bc69eae426e837139
FrankieJ13 <142669841+FrankieJ13@users.noreply.github.com>
2026-06-19 11:19:06 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 window.MRKTNG_CONFIG = {
-  googleSheetUrl: "https://docs.google.com/spreadsheets/d/1Uk3ccDHEvq-zHk_vF02Af-grfwQ6-yrwaCXcQMdOXvA/edit?gid=1401660608#gid=1401660608",
+  googleSheetUrl: "https://docs.google.com/spreadsheets/d/1Uk3ccDHEvq-zHk_vF02Af-grfwQ6-yrwaCXcQMdOXvA",
   gid: "1401660608",
   sheetNames: {
     aggregate: "Все вместе_Факт"
